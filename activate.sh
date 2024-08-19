8190e7df2994ac1db7db357eccffc8ec2f1c5b6f
Saee <saee_saadat@outlook.com>
2024-08-19 17:50:59 +0330

    Fix: zsh and MacOS compatibility

--- activate.sh
+++ activate.sh
@@ -4,26 +4,38 @@
 # . activate.sh
 # . activate.sh <venv-name>
 
+SHELLNAME=$(basename $SHELL)
+if [ "${SHELLNAME}" = "zsh" ]; then
+    HERE=${0:a:h}     
+elif [[ "${SHELLNAME}" = "sh"  ]] || [[ "${SHELLNAME}" = "bash" ]]; then
+    HERE=$(dirname $(realpath ${HERE}))/..
+else
+    echo "${SHELLNAME} is not supported" >2
+    return 1
+fi
+  
 
 get_venv() {
-  local here=$(dirname $(readlink -f ${BASH_SOURCE}))/..
-  local venv=$1
-
-  if [ -z "$venv" ]; then
-    venv=$(grep -r VENV ${here}/Makefile | cut -d'=' -f2 | xargs)
-    if [ -z "${venv}" ]; then
-      venv=$(grep -r PKG_NAME ${here}/Makefile | cut -d'=' -f2 | xargs)
-    fi
-  fi
+   # local thisfile
+   local venv=$1
   
-  if [ -z "${venv}" ]; then
-    echo "Cannot resolve the VENV environment variable"
-    return 1
-  fi
-
-  echo $venv
-}
-
+   if [ -z "$venv" ]; then
+     venv=$(grep -r VENV ${HERE}/Makefile | cut -d'=' -f2 | xargs)
+     if [ -z "${venv}" ]; then
+       venv=$(grep -r PKG_NAME ${HERE}/Makefile | cut -d'=' -f2 | xargs)
+     fi
+   fi
+   
+   if [ -z "${venv}" ]; then
+     echo "Cannot resolve the VENV environment variable"
+     return 1
+   fi
+  
+   echo $venv
+} 
+  
 
 PREFIX=${HOME}/.virtualenvs/$(get_venv $1)
 source ${PREFIX}/bin/activate
+unset HERE
+unset PREFIX
