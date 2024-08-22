b477b5a089f8a3c6b71ce339136cd40eb9586f3d
a.alijan <a.alijan>
2024-08-22 16:49:44 +0330

    feature: add condition to checking is activate.sh sourced before or not

--- activate.sh
+++ activate.sh
@@ -4,38 +4,52 @@
 # . activate.sh
 # . activate.sh <venv-name>
 
+deactivation() {
+  if [ -n "$VIRTUAL_ENV" ]; then
+    if command -v deactivate > /dev/null; then
+      deactivate
+      unset _ACTIVATE_SCRIPT_SOURCED
+      unalias deactivate 2>/dev/null || true
+      echo "Virtual environment deactivated."
+    else
+      echo "Deactivate command not found. You may need to manually deactivate."
+    fi
+  else
+    echo "No virtual environment is currently active."
+  fi
+}
 
-SHELLNAME=$(basename $SHELL)
-if [ "${SHELLNAME}" = "zsh" ]; then
-    HERE=${0:a:h}     
-elif [[ "${SHELLNAME}" = "sh"  ]] || [[ "${SHELLNAME}" = "bash" ]]; then
-    HERE=$(dirname $(readlink -f ${BASH_SOURCE}))/..
-else
-    echo "${SHELLNAME} is not supported" >&2
-    return 1
+if [ -n "$_ACTIVATE_SCRIPT_SOURCED" ]; then
+  echo "This script has already been sourced and cannot be sourced again."
+  return 1
 fi
-  
+
+export _ACTIVATE_SCRIPT_SOURCED=true
 
 get_venv() {
-   local venv=$1
+  local here=$(dirname $(readlink -f ${BASH_SOURCE}))/..
+  local venv=$1
+
+  if [ -z "$venv" ]; then
+    venv=$(grep -r VENV ${here}/Makefile | cut -d'=' -f2 | xargs)
+    if [ -z "${venv}" ]; then
+      venv=$(grep -r PKG_NAME ${here}/Makefile | cut -d'=' -f2 | xargs)
+    fi
+  fi
   
-   if [ -z "$venv" ]; then
-     venv=$(grep -w VENV_NAME ${HERE}/Makefile | cut -d'=' -f2 | xargs)
-     if [ -z "${venv}" ]; then
-       venv=$(grep -w PKG_NAME ${HERE}/Makefile | cut -d'=' -f2 | xargs)
-     fi
-   fi
-   
-   if [ -z "${venv}" ]; then
-     echo "Cannot resolve the VENV_NAME environment variable"
-     return 1
-   fi
-  
-   echo $venv
-} 
-  
+  if [ -z "${venv}" ]; then
+    echo "Cannot resolve the VENV environment variable"
+    return 1
+  fi
+
+  echo $venv
+}
+
 
 PREFIX=${HOME}/.virtualenvs/$(get_venv $1)
 source ${PREFIX}/bin/activate
+
+alias deactivate=deactivation
+
 unset HERE
 unset PREFIX
