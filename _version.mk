8b9558a11ba37cae3b8bbf86725204f1b2db944f
pylover <vahid.mardani@gmail.com>
2024-08-26 17:35:36 +0330

    v1.5.2

--- _version.mk
+++ _version.mk
@@ -1,5 +1,5 @@
 PYTHON_MAKELIB_URL ?= https://github.com/pylover/python-makelib
-PYTHON_MAKELIB_VERSION = 1.5.1
+PYTHON_MAKELIB_VERSION = 1.5.2
 
 
 version_greater_equal = $(shell if printf '%s\n%s\n' '$(1)' \
